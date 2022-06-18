6589237aeee6a83e8b460dcc450333db14f11781
andreasvidal <andrea.s.vidal00@gmail.com>
2022-06-18 17:21:37 -0400

    se muestra mensaje de error input vacio solo nombre

--- src/js/main.js
+++ src/js/main.js
@@ -8,13 +8,13 @@ const inputNameUser = d.getElementById('inputNameUser');
 
 btnSend.addEventListener('click', (e) => {
     e.preventDefault();
+    //API de validación de restricciones HTML5
     inputNameUser.setCustomValidity("");
     inputNameUser.checkValidity();
     console.log(inputNameUser.checkValidity());
     let errorText = nameError.style.display
     if (errorText === 'block') {
         nameError.style.display = 'none'
-            //cambia el mensade el boton
         console.log(errorText)
     } else {
         nameError.style.display = 'block'
